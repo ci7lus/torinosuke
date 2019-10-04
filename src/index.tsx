5ec69bcd19dceefe357c182d2b87f4b0c8959012
HANATANI Takuma <mail@potato4d.me>
2019-10-04 17:57:55 +0900

    chore: Directly import useState instead of destructuring object

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-const { useState } = React
+import { useState } from "react"
 import { render } from "react-dom"
 import { OAuth } from "oauth"
 import axios from "axios"
